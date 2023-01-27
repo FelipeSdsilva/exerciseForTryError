cd20e6f290c76093062c0e65ed30fa71995e16e5
Felipe Sousa <felipe.fps09@hotmail.com>
2023-01-27 16:48:44 -0300

    Clean class Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,34 +1,14 @@
 package application;
 
-import application.view.AccountViewText;
+import application.view.InitialView;
 
 import java.util.Locale;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-        String name;
-        Integer number;
-        Double amount, withdrawLimit;
 
-
-        System.out.print(AccountViewText.enterAccountData + "\n" + AccountViewText.numberAccount);
-        number = sc.nextInt();
-        sc.nextLine();
-
-        System.out.print(AccountViewText.holderName);
-        name = sc.nextLine();
-
-        System.out.print(AccountViewText.InitialBalance);
-        amount = sc.nextDouble();
-
-        System.out.print(AccountViewText.withdrawLimit);
-        withdrawLimit = sc.nextDouble();
-
-
-        sc.close();
+        InitialView.startProgram();
     }
 }
